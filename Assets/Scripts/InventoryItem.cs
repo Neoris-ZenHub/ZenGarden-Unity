8bd6283d9215adca8d78a6e1b7da277d9aa7c497
EAM037 <A01722796@tec.mx>
2024-04-11 23:00:37 -0600

    Cambios en el InventoryItem

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -18,7 +16,14 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnDrag(PointerEventData eventData){
         Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
+        // Convert mouse position to world position
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Keep the same z-coordinate to maintain the same depth
+        mousePosition.x = transform.position.x;
+        mousePosition.y = transform.position.y;
+        mousePosition.z = transform.position.z;
+        // Set the position of the object to the mouse position
+        transform.position = mousePosition;
     }
 
     public void OnEndDrag (PointerEventData eventData){
