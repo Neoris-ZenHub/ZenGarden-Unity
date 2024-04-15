ca5c40d194ea394235d1a2c264b0fca5e7346cbc
Emmanuel Acosta <a01722796@tec.mx>
2024-04-15 12:52:01 -0600

    InventoryItem arreglado

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -6,25 +6,22 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 {   
     public Image image;
     [HideInInspector] public Transform parentAfterDrag;
-
+    
     public void OnBeginDrag (PointerEventData eventData){
         Debug.Log("Begin drag");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
         image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData){
         Debug.Log("Dragging");
-        // Convert mouse position to world position
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Keep the same z-coordinate to maintain the same depth
-        mousePosition.x = transform.position.x;
-        mousePosition.y = transform.position.y;
-        mousePosition.z = transform.position.z;
-        // Set the position of the object to the mouse position
-        transform.position = mousePosition;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.nearClipPlane;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
     }
+
 
     public void OnEndDrag (PointerEventData eventData){
         Debug.Log("End drag");
