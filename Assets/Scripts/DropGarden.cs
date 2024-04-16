32ac20326828588ac26ed9df907831ce0fc8ac73
EAM037 <A01722796@tec.mx>
2024-04-15 19:20:09 -0600

    Flore incluidas

--- Assets/Scripts/DropGarden.cs
+++ Assets/Scripts/DropGarden.cs
@@ -7,19 +7,16 @@ public class DropGarden : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        if (transform.childCount == 0)
-        {
-            GameObject dropped = eventData.pointerDrag;
-            InventoryItem draggableItem = dropped.GetComponent<InventoryItem>();
-            draggableItem.parentAfterDrag = transform;
+        GameObject dropped = eventData.pointerDrag;
+        InventoryItem draggableItem = dropped.GetComponent<InventoryItem>();
+        draggableItem.parentAfterDrag = transform;
 
-            // Get random coordinates within the specified range
-            float randomX = Random.Range(-8.61f, 9.01f);
-            float randomY = Random.Range(-2.77f, 4.44f);
-            float randomZ = 0f; // Assuming the object remains at the same Z-coordinate
+        // Get random coordinates within the specified range
+        float randomX = Random.Range(-8.61f, 9.01f);
+        float randomY = Random.Range(-2.77f, 4.44f);
+        float randomZ = 0f; // Assuming the object remains at the same Z-coordinate
 
-            // Set the position of the dropped object to the random coordinates
-            dropped.transform.position = new Vector3(randomX, randomY, randomZ);
-        }
+        // Set the position of the dropped object to the random coordinates
+        dropped.transform.position = new Vector3(randomX, randomY, randomZ);
     }
 }
