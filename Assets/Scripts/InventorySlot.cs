2e319ad8a4bf4a2c8aeb6af8ffb98d25cf1e1c58
mykrex <marymmartynez@gmail.com>
2024-04-18 15:16:58 -0600

    showing selected Slot

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour, IDropHandler
+public class InventorySlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Image image;
-    //public Color selectedColor, notSelectedColor;
-    /*
-    private void Awake()
-    {
+    public Color selectedColor, notSelectedColor;
+
+    private void Awake(){
         Deselect();
     }
-    public void Select()
-    {
+
+    public void OnPointerEnter(PointerEventData eventData){
+        Select();
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        Deselect();
+    }
+
+    public void Select(){
         image.color = selectedColor;
     }
 
-    public void Deselect()
-    {
+    public void Deselect(){
         image.color = notSelectedColor;
     }
-    */
-    public void OnDrop(PointerEventData eventData){
-        if (transform.childCount == 0){
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
             GameObject dropped = eventData.pointerDrag;
             InventoryItem draggableItem = dropped.GetComponent<InventoryItem>();
             draggableItem.parentAfterDrag = transform;
